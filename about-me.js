bd5ec6b671d196e390dcc9d2c8cd8c9d2e23bc32
Rémi Tiab <remitiab@hotmail.fr>
2024-01-25 11:23:51 +0100

    Update github profile

---
+++ about-me.js
@@ -0,0 +1,25 @@
+class Remi extends FullStackDeveloper {
+  getPersonalInfo = () => ({
+    aboutMe: `
+      🗺️ The experience of traveling,
+      🩸 The pain of tattoos,
+      🏍️ The adrenaline of motorcycling,
+      💪 The sweat of bodybuilding,
+      📚 The calm of reading,
+      🖼️ The madness of art,
+      🎹 The deep inspiration of multiverse music,
+    `,
+
+    name: 'Rémi Tiab',
+    age: '30',
+    from: 'France',
+
+    email: 'remitiab@outlook.fr',
+    linkedin: '',
+    github: '',
+
+    livedIn: {
+      Pais: 'Until now',
+    },
+  });
+}
